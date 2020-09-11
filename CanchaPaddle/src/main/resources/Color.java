c0fae4fc1ebb2dbb6fdd0769a904587f85b5e34a
bmangia <bmangia@localhost>
2020-09-11 17:24:03 -0300

    asd

---
+++ CanchaPaddle/src/main/resources/Color.java
@@ -0,0 +1,17 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Colores")
+public class Color {
+	    @Id
+	    @GeneratedValue
+	    private Long codigo;
+
+	    
+	    private String descripcion;
+
+}
+
